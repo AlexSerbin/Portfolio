aefb8bc48e385ba18f1f6641245cd9f25d430070
Alex.Serbin <daldonchik@gmail.com>
2018-11-01 00:24:30 +0200

    1st commit

--- Automations/SeleniumTests/HtmlTegTest/src/test/java/CheckHTMLTagsTest.java
+++ Automations/SeleniumTests/HtmlTegTest/src/test/java/CheckHTMLTagsTest.java
@@ -10,59 +10,60 @@ import java.util.HashSet;
 import java.util.List;
 
 public class CheckHTMLTagsTest {
-    private static WebDriver seleniumWebDriverInstance;
+    private static WebDriver WebDriver;
     private static String url;
 
     @BeforeClass
     public static void beforeMethod(){
-        seleniumWebDriverInstance= new ChromeDriver();
+        WebDriver= new ChromeDriver();
         url="https://mvnrepository.com/";
-        seleniumWebDriverInstance.get(url);
+        WebDriver.get(url);
     }
+    //
     @Test
     public void testMethodScriptNumber(){
 
-        int sizeOfScriptTags = seleniumWebDriverInstance.findElements(By.xpath("//script[@type='text/javascript']")).size();
+        int sizeOfScriptTags = WebDriver.findElements(By.xpath("//script[@type='text/javascript']")).size();
         assertEquals(1, sizeOfScriptTags);
         System.out.println(sizeOfScriptTags);
     }
     @Test
     public void testMethodScriptWithoutSrc(){
-        int sizeOfAllScripts= seleniumWebDriverInstance.findElements(By.xpath("//script")).size();
-        int sizeOfScriptTagsWithSrc = seleniumWebDriverInstance.findElements(By.xpath("//script[@src]")).size();
+        int sizeOfAllScripts= WebDriver.findElements(By.xpath("//script")).size();
+        int sizeOfScriptTagsWithSrc = WebDriver.findElements(By.xpath("//script[@src]")).size();
         assertEquals(sizeOfScriptTagsWithSrc, sizeOfAllScripts);
     }
     @Test
     public void testMethodCssNumber() {
 
-        int sizeOfCssTags = seleniumWebDriverInstance.findElements(By.xpath("//style[@type='text/css']")).size();
+        int sizeOfCssTags = WebDriver.findElements(By.xpath("//style[@type='text/css']")).size();
         assertEquals(1, sizeOfCssTags);
         System.out.println(sizeOfCssTags);
     }
     @Test
     public void testMethodStyleNumber() {
 
-        int sizeOfStyleTags = seleniumWebDriverInstance.findElements(By.xpath("//style")).size();
+        int sizeOfStyleTags = WebDriver.findElements(By.xpath("//style")).size();
         assertEquals(0, sizeOfStyleTags);
         System.out.println(sizeOfStyleTags);
     }
     @Test
     public void testMethodH1Number() {
 
-        int sizeOfH1Tags = seleniumWebDriverInstance.findElements(By.xpath("//h1")).size();
+        int sizeOfH1Tags = WebDriver.findElements(By.xpath("//h1")).size();
         assertEquals(1, sizeOfH1Tags);
         System.out.println(sizeOfH1Tags);
     }
     @Test
     public void testMethodID() {
 
-        List<WebElement> ID = seleniumWebDriverInstance.findElements(By.xpath("//div[@id]"));
-        HashSet<WebElement> UniqID=new HashSet<WebElement>(ID);
-        assertEquals(ID.size(),UniqID.size());
+        List<WebElement> ID = WebDriver.findElements(By.xpath("//div[@id]"));
+        HashSet<WebElement> UniqueID=new HashSet<WebElement>(ID);
+        assertEquals(ID.size(),UniqueID.size());
     }
 
     @AfterClass
     public static void afterMethod(){
-        seleniumWebDriverInstance.quit();
+        WebDriver.quit();
     }
 }
